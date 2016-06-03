bda7198431606a71ec1e9ebb6e7c7950a5d4d7fd
Loïc Liné <line.loic@gmail.com>
2016-06-03 12:07:29 +0200

    Update RegexRouter.php
    added some exceptions

--- RegexRouter.php
+++ RegexRouter.php
@@ -1,20 +1,30 @@
 <?php
-
-class RegexRouter {
-    
+class RegexRouter
+{
     private $routes = array();
-    
-    public function route($pattern, $callback) {
+
+    public function route($pattern, $callback)
+	{
+		if (isset($this->routes[$pattern])) {
+			trigger_error("This route already exists: " . $pattern, E_USER_NOTICE);
+			return;
+		}
+
         $this->routes[$pattern] = $callback;
-    }
-    
-    public function execute($uri) {
+	}
+
+    public function execute($uri)
+	{
         foreach ($this->routes as $pattern => $callback) {
+			if (!is_callable($callback)) {
+				trigger_error("Unknown route.", E_USER_NOTICE);
+				return;
+			}
+
             if (preg_match($pattern, $uri, $params) === 1) {
                 array_shift($params);
                 return call_user_func_array($callback, array_values($params));
             }
         }
     }
-    
 }
